81e27b33212ff7f0b38c4ef61652945e1844c505
guoj <dev.guoj@gmail.com>
2015-02-28 10:50:07 +0800

    A new dict in your terminal

--- terminaldict.py
+++ terminaldict.py
@@ -4,13 +4,13 @@ from urllib import urlopen
 from bs4 import BeautifulSoup as bs
 
 def look_up(word):
-    url = 'http://www.merriam-webster.com/dictionary/'
+    url = 'http://dict.youdao.com/search?q='
     text = urlopen(url+word).read()
     soup = bs(text)
-    return soup.p.get_text()
+    return soup.find('div', {'class': 'trans-container'}).find('ul').get_text()
 
 def run(prompt='> '):
-    print 'Press Ctrl + C to quit\n'
+    print 'Press Ctrl + C to quit.\n'
     while True:
         val = look_up(raw_input(prompt))
         if val is not None: print val
