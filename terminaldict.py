7b9bb0ac4d501e7bcbf17704dbaa04f1687a4e9c
guoj <dev.guoj@gmail.com>
2015-03-09 16:18:27 +0800

    new feature: print unlearned words

--- terminaldict.py
+++ terminaldict.py
@@ -3,9 +3,11 @@
 from urllib import urlopen
 from bs4 import BeautifulSoup as bs
 
+word_list = []
+
 def look_up(word):
     url = 'http://dict.youdao.com/search?q='
-    text = urlopen(url+word).read()
+    text = urlopen(url + word).read()
     soup = bs(text)
     ph = soup.find('div', {'class': 'baav'}).get_text()
     mean = soup.find('div', {'class': 'trans-container'}).find('ul').get_text()
@@ -13,9 +15,15 @@ def look_up(word):
 
 def run(prompt='> '):
     print 'Press Ctrl + C to quit.\n'
-    while True:
-        val = look_up(raw_input(prompt))
-        if val is not None: print val
+    try:
+        while True:
+            w = raw_input(prompt)
+            word_list.append(w)
+            val = look_up(w)
+            if val is not None: print val
+    except KeyboardInterrupt:
+        for i in word_list:
+            print i
 
 if __name__ == '__main__':
     run()
